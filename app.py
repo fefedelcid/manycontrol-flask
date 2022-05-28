748a7ed0748c9254f3c0846df9c148382990d950
Federico del Cid <thegreench1996@gmail.com>
2022-05-28 03:28:28 -0300

    .

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-# importar rutas
-from modules import routes
-from modules import errorhandler
 
 if __name__=='__main__':
+    # importar rutas
+    from modules import routes
+    from modules import errorhandler
+
     # Ejecutar servidor
     app.run(debug=True)

0efa4427441d5fc824a7558577ed845f086d168f
Federico del Cid <thegreench1996@gmail.com>
2022-05-29 20:13:02 -0300

    bs endpoint

---
+++ modules/bs_endpoints.py
@@ -0,0 +1,6 @@
+from .app import app
+from flask import render_template
+
+@app.route('/bs')
+def index_bs():
+    return render_template('bs/index.html')
--- modules/routes.py
+++ modules/routes.py
@@ -1,6 +1,6 @@
 from .app import app
 from .KeyGen import get
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, response
 
 default_response = {'message':'endpoint is not set'}
 
@@ -16,18 +16,7 @@ def index():
     return render_template('views/mainpage.html')
 
 
-@app.route('/api/keygen')
 @app.route('/temp/keygen')
 def get_key():
     valid_key = get()
     return render_template('views/token.html', token = valid_key)
-
-
-@app.route('/asd')
-@app.route('/asd/<str>')
-def asd(str=None):
-    if str:
-        return jsonify({'message':f'Hola {str}!'})
-    else:
-        return jsonify({'message':'Hola mundo!'})
-    

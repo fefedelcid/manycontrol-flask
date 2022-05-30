1de83418e11b02495288749898a52e3810c87724
Federico del Cid <thegreench1996@gmail.com>
2022-05-30 05:33:25 -0300

    added bs endpoint html

--- modules/bs_endpoints.py
+++ modules/bs_endpoints.py
@@ -7,35 +7,42 @@ from math import ceil
 
 api_url = "https://brawlify.com/stats/profile/"
 
-@app.route('/bs/<tag>', methods=['GET', 'POST'])
+@app.route('/bs/<tag>/', methods=['GET', 'POST'])
 def get_brawlers(tag):
     req = get(api_url+tag)
     bs = BeautifulSoup(req.content, 'html.parser')
 
-    try:
-        brawlers = []
-        for brawl in bs.find_all('a', class_='brawlerBlock'):
-            if int(brawl.find_all('div')[-1].text)>=493:
-                brawlers.append({
-                    'name':brawl.find('img')['alt'],
-                    'trophies':int(brawl.find_all('div')[-1].text),
-                    'img_url':brawl.find('img')['src']
-                })
+    brawlers = []
+    for brawl in bs.find_all('a', class_='brawlerBlock'):
+        if int(brawl.find_all('div')[-1].text)>=493:
+            brawlers.append({
+                'name':brawl.find('img')['alt'],
+                'trophies':int(brawl.find_all('div')[-1].text),
+                'img_url':brawl.find('img')['src']
+            })
 
-        for brawl in brawlers:
-            diff = brawl['trophies']%25
-            at_end = brawl['trophies']-diff-1 if brawl['trophies']>525 else brawl['trophies']-diff
-            brawl['next_goal'] = brawl['trophies']+25-diff
-            brawl['remaining_wins'] = ceil((25-diff)/8)
-            brawl['trophies_at_end'] = at_end
-            brawl['trophies_losted'] = brawl['trophies']-at_end
+    for brawl in brawlers:
+        diff = brawl['trophies']%25
+        if brawl['trophies']>1000:
+            continue
+        elif brawl['trophies']>524:
+            at_end = brawl['trophies']-diff-1
+        else:
+            at_end = brawl['trophies']-diff
 
-        return jsonify(brawlers)
-    except:
-        return jsonify({'message':'invalid tag'})
+        brawl['remaining_wins'] = ceil((25-diff)/8)
+        brawl['next_goal'] = brawl['trophies']+25-diff
+        brawl['next_step'] = brawl['trophies']+brawl['remaining_wins']*8
+        brawl['trophies_at_end'] = at_end
+        brawl['trophies_losted'] = brawl['trophies']-at_end
+
+    if len(brawlers)>0:
+        return render_template('bs/calculator.html', brawlers=brawlers)
+
+    return jsonify({'message':'invalid tag'})
 
 
-@app.route('/bs')
+@app.route('/bs/')
 def index_bs():
     return render_template('bs/index.html')
 
